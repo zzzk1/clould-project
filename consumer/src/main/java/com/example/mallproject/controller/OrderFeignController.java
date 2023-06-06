9a82e3a3ea810c3fe18e8545a170768b98cf1243
zzzk1 <36791902+zzzk1@users.noreply.github.com>
2023-06-06 13:22:00 +0800

    order

--- consumer/src/main/java/com/example/mallproject/controller/OrderFeignController.java
+++ consumer/src/main/java/com/example/mallproject/controller/OrderFeignController.java
@@ -1,16 +1,52 @@
 package com.example.mallproject.controller;
 
 import api.Result;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.mallproject.entity.Order;
+import com.example.mallproject.entity.dto.OrderDTO;
+import com.example.mallproject.feign.OrderFeignService;
 import com.example.mallproject.feign.TestFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author zzzk1
  */
 @RestController
+@RequestMapping("order")
 public class OrderFeignController {
+    @Autowired
+    private OrderFeignService orderService;
 
+    @GetMapping("/page")
+    public Result<Page<Order>> getAll(@RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+                                      @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
+                                      @RequestParam(value = "username", defaultValue = "") String username) {
+        return orderService.getAll(pageNum, pageSize, username);
+    }
 
+    @PostMapping
+    public Result<Boolean> edit(@RequestBody Order order) {
+        return orderService.edit(order);
+    }
+
+    @DeleteMapping("{id}")
+    public Result<Boolean> delete(@PathVariable long id) {
+        return orderService.delete(id);
+    }
+
+    @PostMapping("/del/batch")
+    public Result<Boolean> deleteByIds(@RequestBody List<Long> ids) {
+        return orderService.deleteByIds(ids);
+    }
+
+    @GetMapping("/front/page/{username}")
+    public Result<Page<OrderDTO>> getFrontAll(@RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+                                              @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
+                                              @RequestParam(value = "spuName", defaultValue = "") String spuName,
+                                              @PathVariable String username) {
+        return orderService.getFrontAll(pageNum, pageSize, spuName, username);
+    }
 }
--- consumer/src/main/java/com/example/mallproject/feign/OrderFeignService.java
+++ consumer/src/main/java/com/example/mallproject/feign/OrderFeignService.java
@@ -1,8 +1,33 @@
 package com.example.mallproject.feign;
 
+import api.Result;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.mallproject.entity.Order;
+import com.example.mallproject.entity.dto.OrderDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @FeignClient(value="provider",path = "/order")
 public interface OrderFeignService {
+    @GetMapping("/page")
+    public Result<Page<Order>> getAll(@RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+                                      @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
+                                      @RequestParam(value = "username", defaultValue = "") String username);
 
+    @PostMapping
+    public Result<Boolean> edit(@RequestBody Order order);
+
+    @DeleteMapping("{id}")
+    public Result<Boolean> delete(@PathVariable("id") long id);
+
+    @PostMapping("/del/batch")
+    public Result<Boolean> deleteByIds(@RequestBody List<Long> ids);
+
+    @GetMapping("/front/page/{username}")
+    public Result<Page<OrderDTO>> getFrontAll(@RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+                                              @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
+                                              @RequestParam(value = "spuName", defaultValue = "") String spuName,
+                                              @PathVariable("username") String username);
 }
